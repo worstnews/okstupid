f25a7388724293e57e9a84e8026df80b6f7453ae
steven <steven@vimeo.com>
2013-10-21 19:18:21 -0400

    UEONO

--- rate.js
+++ rate.js
@@ -1,5 +1,5 @@
 var url = 'https://www.okcupid.com/login', 
-    username = '  ', 
+    username = '  ', //don't forget to enter your own credentials ya dummy! 
     password = '  ',
     x = require('casper').selectXPath;
 
@@ -17,6 +17,179 @@ casper.start(url, function () {
         this.test.info('click through to quickmatch');
     });
 
+/* this is ghetto and i'm embarassed.
+   it seems like OKC limits ratings somehow. tried looping it 100x anid it crapped out silently at some point.
+   leaving it at 25 for now
+*/
+
+    casper.then(function() {
+        this.click(x('//*[@id="stars"]/li[5]'));
+        this.wait(3000); // wait before doing anything else  
+        this.echo('you just gave 5 stars to ' + this.evaluate(function() {
+            return __utils__.getElementByXPath('//a[contains(@href, "?cf=quickmatch/")]').getAttribute('href');
+        }), 'INFO'); // print dat shit
+    });
+    casper.then(function() {
+        this.click(x('//*[@id="stars"]/li[5]'));
+        this.wait(3000); // wait before doing anything else  
+        this.echo('you just gave 5 stars to ' + this.evaluate(function() {
+            return __utils__.getElementByXPath('//a[contains(@href, "?cf=quickmatch/")]').getAttribute('href');
+        }), 'INFO'); // print dat shit
+    });
+    casper.then(function() {
+        this.click(x('//*[@id="stars"]/li[5]'));
+        this.wait(3000); // wait before doing anything else  
+        this.echo('you just gave 5 stars to ' + this.evaluate(function() {
+            return __utils__.getElementByXPath('//a[contains(@href, "?cf=quickmatch/")]').getAttribute('href');
+        }), 'INFO'); // print dat shit
+    });
+    casper.then(function() {
+        this.click(x('//*[@id="stars"]/li[5]'));
+        this.wait(3000); // wait before doing anything else  
+        this.echo('you just gave 5 stars to ' + this.evaluate(function() {
+            return __utils__.getElementByXPath('//a[contains(@href, "?cf=quickmatch/")]').getAttribute('href');
+        }), 'INFO'); // print dat shit
+    });
+    casper.then(function() {
+        this.click(x('//*[@id="stars"]/li[5]'));
+        this.wait(3000); // wait before doing anything else  
+        this.echo('you just gave 5 stars to ' + this.evaluate(function() {
+            return __utils__.getElementByXPath('//a[contains(@href, "?cf=quickmatch/")]').getAttribute('href');
+        }), 'INFO'); // print dat shit
+    });
+    casper.then(function() {
+        this.click(x('//*[@id="stars"]/li[5]'));
+        this.wait(3000); // wait before doing anything else  
+        this.echo('you just gave 5 stars to ' + this.evaluate(function() {
+            return __utils__.getElementByXPath('//a[contains(@href, "?cf=quickmatch/")]').getAttribute('href');
+        }), 'INFO'); // print dat shit
+    });
+    casper.then(function() {
+        this.click(x('//*[@id="stars"]/li[5]'));
+        this.wait(3000); // wait before doing anything else  
+        this.echo('you just gave 5 stars to ' + this.evaluate(function() {
+            return __utils__.getElementByXPath('//a[contains(@href, "?cf=quickmatch/")]').getAttribute('href');
+        }), 'INFO'); // print dat shit
+    });
+    casper.then(function() {
+        this.click(x('//*[@id="stars"]/li[5]'));
+        this.wait(3000); // wait before doing anything else  
+        this.echo('you just gave 5 stars to ' + this.evaluate(function() {
+            return __utils__.getElementByXPath('//a[contains(@href, "?cf=quickmatch/")]').getAttribute('href');
+        }), 'INFO'); // print dat shit
+    });
+    casper.then(function() {
+        this.click(x('//*[@id="stars"]/li[5]'));
+        this.wait(3000); // wait before doing anything else  
+        this.echo('you just gave 5 stars to ' + this.evaluate(function() {
+            return __utils__.getElementByXPath('//a[contains(@href, "?cf=quickmatch/")]').getAttribute('href');
+        }), 'INFO'); // print dat shit
+    });
+    casper.then(function() {
+        this.click(x('//*[@id="stars"]/li[5]'));
+        this.wait(3000); // wait before doing anything else  
+        this.echo('you just gave 5 stars to ' + this.evaluate(function() {
+            return __utils__.getElementByXPath('//a[contains(@href, "?cf=quickmatch/")]').getAttribute('href');
+        }), 'INFO'); // print dat shit
+    });
+    casper.then(function() {
+        this.click(x('//*[@id="stars"]/li[5]'));
+        this.wait(3000); // wait before doing anything else  
+        this.echo('you just gave 5 stars to ' + this.evaluate(function() {
+            return __utils__.getElementByXPath('//a[contains(@href, "?cf=quickmatch/")]').getAttribute('href');
+        }), 'INFO'); // print dat shit
+    });
+    casper.then(function() {
+        this.click(x('//*[@id="stars"]/li[5]'));
+        this.wait(3000); // wait before doing anything else  
+        this.echo('you just gave 5 stars to ' + this.evaluate(function() {
+            return __utils__.getElementByXPath('//a[contains(@href, "?cf=quickmatch/")]').getAttribute('href');
+        }), 'INFO'); // print dat shit
+    });
+    casper.then(function() {
+        this.click(x('//*[@id="stars"]/li[5]'));
+        this.wait(3000); // wait before doing anything else  
+        this.echo('you just gave 5 stars to ' + this.evaluate(function() {
+            return __utils__.getElementByXPath('//a[contains(@href, "?cf=quickmatch/")]').getAttribute('href');
+        }), 'INFO'); // print dat shit
+    });
+    casper.then(function() {
+        this.click(x('//*[@id="stars"]/li[5]'));
+        this.wait(3000); // wait before doing anything else  
+        this.echo('you just gave 5 stars to ' + this.evaluate(function() {
+            return __utils__.getElementByXPath('//a[contains(@href, "?cf=quickmatch/")]').getAttribute('href');
+        }), 'INFO'); // print dat shit
+    });
+    casper.then(function() {
+        this.click(x('//*[@id="stars"]/li[5]'));
+        this.wait(3000); // wait before doing anything else  
+        this.echo('you just gave 5 stars to ' + this.evaluate(function() {
+            return __utils__.getElementByXPath('//a[contains(@href, "?cf=quickmatch/")]').getAttribute('href');
+        }), 'INFO'); // print dat shit
+    });
+    casper.then(function() {
+        this.click(x('//*[@id="stars"]/li[5]'));
+        this.wait(3000); // wait before doing anything else  
+        this.echo('you just gave 5 stars to ' + this.evaluate(function() {
+            return __utils__.getElementByXPath('//a[contains(@href, "?cf=quickmatch/")]').getAttribute('href');
+        }), 'INFO'); // print dat shit
+    });
+    casper.then(function() {
+        this.click(x('//*[@id="stars"]/li[5]'));
+        this.wait(3000); // wait before doing anything else  
+        this.echo('you just gave 5 stars to ' + this.evaluate(function() {
+            return __utils__.getElementByXPath('//a[contains(@href, "?cf=quickmatch/")]').getAttribute('href');
+        }), 'INFO'); // print dat shit
+    });
+    casper.then(function() {
+        this.click(x('//*[@id="stars"]/li[5]'));
+        this.wait(3000); // wait before doing anything else  
+        this.echo('you just gave 5 stars to ' + this.evaluate(function() {
+            return __utils__.getElementByXPath('//a[contains(@href, "?cf=quickmatch/")]').getAttribute('href');
+        }), 'INFO'); // print dat shit
+    });
+    casper.then(function() {
+        this.click(x('//*[@id="stars"]/li[5]'));
+        this.wait(3000); // wait before doing anything else  
+        this.echo('you just gave 5 stars to ' + this.evaluate(function() {
+            return __utils__.getElementByXPath('//a[contains(@href, "?cf=quickmatch/")]').getAttribute('href');
+        }), 'INFO'); // print dat shit
+    });
+    casper.then(function() {
+        this.click(x('//*[@id="stars"]/li[5]'));
+        this.wait(3000); // wait before doing anything else  
+        this.echo('you just gave 5 stars to ' + this.evaluate(function() {
+            return __utils__.getElementByXPath('//a[contains(@href, "?cf=quickmatch/")]').getAttribute('href');
+        }), 'INFO'); // print dat shit
+    });
+    casper.then(function() {
+        this.click(x('//*[@id="stars"]/li[5]'));
+        this.wait(3000); // wait before doing anything else  
+        this.echo('you just gave 5 stars to ' + this.evaluate(function() {
+            return __utils__.getElementByXPath('//a[contains(@href, "?cf=quickmatch/")]').getAttribute('href');
+        }), 'INFO'); // print dat shit
+    });
+    casper.then(function() {
+        this.click(x('//*[@id="stars"]/li[5]'));
+        this.wait(3000); // wait before doing anything else  
+        this.echo('you just gave 5 stars to ' + this.evaluate(function() {
+            return __utils__.getElementByXPath('//a[contains(@href, "?cf=quickmatch/")]').getAttribute('href');
+        }), 'INFO'); // print dat shit
+    });
+    casper.then(function() {
+        this.click(x('//*[@id="stars"]/li[5]'));
+        this.wait(3000); // wait before doing anything else  
+        this.echo('you just gave 5 stars to ' + this.evaluate(function() {
+            return __utils__.getElementByXPath('//a[contains(@href, "?cf=quickmatch/")]').getAttribute('href');
+        }), 'INFO'); // print dat shit
+    });
+    casper.then(function() {
+        this.click(x('//*[@id="stars"]/li[5]'));
+        this.wait(3000); // wait before doing anything else  
+        this.echo('you just gave 5 stars to ' + this.evaluate(function() {
+            return __utils__.getElementByXPath('//a[contains(@href, "?cf=quickmatch/")]').getAttribute('href');
+        }), 'INFO'); // print dat shit
+    });
     casper.then(function() {
         this.click(x('//*[@id="stars"]/li[5]'));
         this.wait(3000); // wait before doing anything else  
